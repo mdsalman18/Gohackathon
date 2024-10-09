b90719a9be2b43cc7f90de76517a6367582129df
MD.Salman <157896817+mdsalman18@users.noreply.github.com>
2024-10-09 14:05:22 +0530

    Update views.py

--- server/Internship/views.py
+++ server/Internship/views.py
@@ -5,9 +5,6 @@ from .serializers import InternshipRegistrationSerializer
 class InternshipRegistrationList(generics.ListCreateAPIView):
     queryset = InternshipRegistration.objects.all()
     serializer_class = InternshipRegistrationSerializer
-    def perform_create(self, serializer):
-        # Save the registration
-        instance = serializer.save()
-
+    
 
 
